785e999db45029a12b221b7109d97c5096e04e88
DracoMindz <704@holbertonschool.com>
2019-05-20 03:25:37 +0000

    FIFO,LIFO

--- monty_main.c
+++ monty_main.c
@@ -1,49 +1,81 @@
 #include "monty.h"
 /**
  * main - take in arguments, read, execute
- *
  * @argc: int argument
  * @argv: pointer to string argument
  * Return: exit Failure if Failure,
  */
 int main(int argc, char *argv[])
 {
-	char *lineptr = NULL;
+	char *lineptr = NULL, *opcode;
 	stack_t **stack = NULL;
-	unsigned int line_number = 0;
+	unsigned int line_number = 0, opnum;
 	size_t n = 0;
 	File *opfi;
-	char *opcode;
-	unsigned int opnum;
 
-
-	if ( argc != 2) /* number of argc*/
+	if (argc != 2) /* number of argc*/
 	{
-		fprintf(stderr,"USAGE: %s\n", argv[0]);/*check, program name*/
+		fprintf(stderr, "USAGE: %s\n", argv[0]);/*check, program name*/
 		exit(EXIT_FAILURE);
 	}
 	opfi = fopen(argv[1], "r"); /*read file*/
-	if (opfi == NULL);
+	if (opfi == NULL)
 	{
 		fprintf("Error: Can't open file %s.\n)", argv[1]);
 		exit(EXIT_FAILURE);
 	}
-	while (getline(lineptr, &n , opfi) != -1);
+	while (getline(lineptr, &n, opfi) != -1)
 	{
-  		sscanf(lineptr, %s %i, opcode, &opnum "\n");
+		sscanf(lineptr, % s % i, opcode, &opnum);
 		line_number++;
-		if (opcode != NULL && &opnum != NULL);
+		if (n < 0)
+		{
+			free(opcode);
+			free(&opnum);
+		}
+		if (opcode != NULL && &opnum != NULL)
 		{
 			fprintf("L%d: unknown instruction opcode", line_number);
 			exit(EXIT_FAILURE);
 		}
-	       	else
+		else
 		{
-	       	*get_op_funct
+			(*get_op_func(opcode, &opnum))(stack_t **stack, unsigned int line_number);
 		}
-		lineptr = NULL
+		lineptr = NULL;
 	}
-	free(new_element);
 	free(lineptr);
 	fclose(opfi);
 	return (0);
+}
+
+/**
+ * get_op_func - function calls functions
+ *
+ * @opcode: chraracter string passed from main
+ * @opnum: unsigned int passed from main
+ *
+ * Return: NULL
+ */
+void *get_op_func(opcode, opnum)
+{
+	instruct_t ops[] = {
+		{"push", push},
+		{"pall", pall},
+		{"pint", pint},
+		{"pop", pop},
+		{"swap", swap},
+		{"add", add},
+		{"nop", nop},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	while (ops[i].op != NULL)
+	{
+		if (*ops[i].op == *s)
+			return (ops[i].f);
+		i++;
+	}
+	return (NULL);
+}
