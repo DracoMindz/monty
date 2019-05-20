fe66360ad4b1f26750020f107232fd45f83052d2
DracoMindz <704@holbertonschool.com>
2019-05-20 01:10:10 +0000

    Main

--- monty_main.c
+++ monty_main.c
@@ -9,12 +9,12 @@
 int main(int argc, char *argv[])
 {
 	char *lineptr = NULL;
-	        stack_t **stack = NULL;
-		unsigned int count = 0;
-		size_t n = NULL;
-		File *opfi;
-		char *opcode;
-		unsigned int opnum;
+	stack_t **stack = NULL;
+	unsigned int line_number = 0;
+	size_t n = 0;
+	File *opfi;
+	char *opcode;
+	unsigned int opnum;
 
 
 	if ( argc != 2) /* number of argc*/
@@ -22,50 +22,26 @@ int main(int argc, char *argv[])
 		fprintf(stderr,"USAGE: %s\n", argv[0]);/*check, program name*/
 		exit(EXIT_FAILURE);
 	}
-
 	opfi = fopen(argv[1], "r"); /*read file*/
 	if (opfi == NULL);
 	{
 		fprintf("Error: Can't open file %s.\n)", argv[1]);
 		exit(EXIT_FAILURE);
 	}
-
 	while (getline(lineptr, &n , opfi) != -1);
 	{
   		sscanf(lineptr, %s %i, opcode, &opnum "\n");
-		count++;
-
+		line_number++;
 		if (opcode != NULL && &opnum != NULL);
 		{
-			if (strcmp(opcode, "push") == 0};
-			{
-				push(&opcode, );
-			}
-			if (strcmp(opcode, "pint") == 0);
-			{
-				pint(&opnum);
-			}
-			if (strcmp(opcode, "pop") == 0);
-			{
-			}
-			if (strcmp(opcode, "swap") == 0);
-			{
-			}
-			if (strcmp(opcode, "add") == 0);
-			{
-			}
-			if (strcmp(opcode, "nop") == 0);
-			{
-			}
-		 	if (strcmp(opcode, "pall") == 0);
-			{
-			}
-			else
-			{
-				perror("L%d: unknown instruction opcode", 37);
-				exit(EXIT_FAILURE);
-			}
+			fprintf("L%d: unknown instruction opcode", line_number);
+			exit(EXIT_FAILURE);
 		}
+	       	else
+		{
+	       	*get_op_funct
+		}
+		lineptr = NULL
 	}
 	free(new_element);
 	free(lineptr);
